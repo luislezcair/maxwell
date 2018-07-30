fe140c1ead4b38cdd2599fa26731cdf2b875426c
Luis Lezcano Airaldi <luislezcair@gmail.com>
2018-07-30 20:57:03 -0300

    [sys] Pantalla de login

--- app/javascript/packs/landing.js
+++ app/javascript/packs/landing.js
@@ -1,11 +1,11 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 // Javacript modules //
 import 'bootstrap';
 
 // SASS stylesheets modules //
 import 'bootstrap/scss/bootstrap.scss';
 import 'font-awesome/scss/font-awesome.scss';
+import '@coreui/coreui/scss/coreui.scss';
+
+import '../stylesheets/landing.scss';
+
+import '../images/brand/10_full.jpg';
